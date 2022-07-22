9e219974f3f5583f695ea9c1d392e4980e226f49
Md. Mehedi Hasan <106642643+0mehedihasan@users.noreply.github.com>
2022-07-22 13:57:54 +0600

    Add files via upload

---
+++ pass-or-fail.py.c
@@ -0,0 +1,8 @@
+result= float(input("Enter your result:"))
+if result>=33:
+        print("Pass")
+        print("Congratulations")
+else:
+        print("Fail")
+        print("Try another time")
+print("your Enquiry done")
